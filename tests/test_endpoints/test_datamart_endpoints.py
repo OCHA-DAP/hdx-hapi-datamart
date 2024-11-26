99d26e51ba9d284fa674d27b0f1acfa6f8762e2a
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-11-26 09:16:46 +0000

    HDX-1385 Test paging of the list endpoint

--- tests/test_endpoints/test_datamart_endpoints.py
+++ tests/test_endpoints/test_datamart_endpoints.py
@@ -2,6 +2,8 @@ import pytest
 import logging
 
 from httpx import AsyncClient
+from sqlalchemy import True_
+from hdx_hapi.datamart.datamart_responses import ListTypeEnum
 from main import app
 from tests.test_endpoints.datamart_endpoint_data import datamart_endpoint_data
 
@@ -50,7 +52,7 @@ async def test_get_with_query_params(event_loop, endpoint):
 @pytest.mark.asyncio
 async def test_get_search(event_loop):
     log.info('started datamart search test')
-    params = {'filter_query': r'dataset_source:ETH\ Zurich\ Climada', 'limit': 10}
+    params = {'query': r'dataset_source:ETH\ Zurich\ Climada', 'limit': 10}
     async with AsyncClient(app=app, base_url='http://test', params=params) as ac:
         response = await ac.get('/api/v1/datamart/search')
 
@@ -87,3 +89,30 @@ async def test_get_data_by_resource_and_dataset_name(event_loop):
         response = await ac.get('/api/v1/datamart/data')
 
     assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_list_paging(event_loop):
+    log.info('Started datamart data by resource and dataset name ')
+    params = {'list_type': ListTypeEnum.ISO3_COUNTRY_CODES.value, 'limit': 300}
+    async with AsyncClient(app=app, base_url='http://test', params=params) as ac:
+        single_page_response = await ac.get('/api/v1/datamart/list')
+    assert single_page_response.status_code == 200
+
+    assert len(single_page_response.json()['data']) == 249
+
+    offset = 0
+    composite = []
+    while True:
+        params = {'list_type': ListTypeEnum.ISO3_COUNTRY_CODES.value, 'limit': 50, 'offset': offset}
+        async with AsyncClient(app=app, base_url='http://test', params=params) as ac:
+            print(params, flush=True)
+            page_response = await ac.get('/api/v1/datamart/list')
+        assert page_response.status_code == 200
+        composite.extend(page_response.json()['data'])
+        offset = page_response.json()['paging_metadata']['next_offset']
+
+        if offset is None:
+            break
+
+    assert len(single_page_response.json()['data']) == len(composite)
